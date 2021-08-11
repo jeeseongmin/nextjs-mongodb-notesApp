2897a921b16f5a9a6ef68e716dd6cbcc767931d8
21500706 <21500706@handong.edu>
2021-08-11 20:17:46 +0900

    dbConnect 2

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
 	reactStrictMode: true,
 	env: {
-		MONGO_URI:
-			"mongodb+srv://master:n7OWPKrNsp1mXcAg@cluster0.zcdex.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+		MONGO_URI: "",
 	},
 };
