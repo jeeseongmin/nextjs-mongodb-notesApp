e41cd1068e6d14d73c9708f77bb802f560decf1e
21500706 <21500706@handong.edu>
2021-08-11 20:17:05 +0900

    edit gitignore

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
 	reactStrictMode: true,
 	env: {
-		MONGO_URI: "",
+		MONGO_URI:
+			"mongodb+srv://master:n7OWPKrNsp1mXcAg@cluster0.zcdex.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
 	},
 };
